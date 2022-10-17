36b0699114d9c1bdd1117fe58f8dcdb50019ae10
Bernd Geiser <bg@ferncast.de>
2022-10-17 14:30:35 +0200

    fix pat decoding

--- mpegts/mpegts_demuxer.cpp
+++ mpegts/mpegts_demuxer.cpp
@@ -39,7 +39,7 @@ uint8_t MpegTsDemuxer::decode(SimpleBuffer &rIn) {
                 if (mPatBuf.size() >= mPatHeader.getSectionLength(mPatBuf)) {
                     mPatHeader.decode(mPatBuf);
                     auto start = rIn.pos();
-                    while(mPatBuf.pos() < start + mPatHeader.mSectionLength - 12) {
+                    while(mPatBuf.pos() < start + mPatHeader.mSectionLength - 8) {
                         auto progNo = mPatBuf.read2Bytes();
                         auto pmtPid = mPatBuf.read2Bytes() & 0x1fff;
                         mPmtMap[pmtPid] = { {}, false, -1, progNo, {} };
